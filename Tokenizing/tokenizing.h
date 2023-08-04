7d63e26d0f12b7d051e8318e449142c8f627d4fc
PrEEtPatEl44 <123954310+PrEEtPatEl44@users.noreply.github.com>
2023-08-04 07:10:28 -0400

    Add files via upload

---
+++ Tokenizing/tokenizing.h
@@ -0,0 +1,8 @@
+// TOKENIZING MODULE HEADER 
+#ifndef _TOKENIZING_H_  //Header guard
+#define _TOKENIZING _H_ //defination of the header guard
+#include <stdio.h> //include standard input/output library for printf
+#include <string.h> //include string library for sting function which strtok used in this module
+void tokenizing(void); //function prototype for the tokenizing function
+#endif //End of Header guard
+
